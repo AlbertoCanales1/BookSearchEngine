85bbceb8f2643b7d15bcb567947056b4e12aac48
AlbertoCanales1 <albertocanales1@outlook.com>
2021-10-07 02:09:50 -0500

    got the application working

--- server/server.js
+++ server/server.js
@@ -2,38 +2,88 @@ const express = require('express');
 const path = require('path');
 const {ApolloServer} = require('apollo-server-express');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  })
 
-server.applyMiddleware({ app });
+  await server.start()
 
 
+  server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
 
-// app.use(routes);
-
-app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
 });
+}
+
+startApolloServer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// server.applyMiddleware({ app });
+
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// // app.use(routes);
+
+// app.get('*', (req, res) =>{
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'))
+// })
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`));
+// });
 
 
